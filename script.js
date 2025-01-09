19399d7a309a0fd8a8d74850eb0833f0158becaa
ronitdalal27 <dalalronit131@gmail.com>
2025-01-09 12:46:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,22 +1,10 @@
 function firstWord(str) {
-      // Check if the input string is empty
-    if (str.length === 0) {
-        return str;
-    }
-
-    // Find the index of the first space
-    const spaceIndex = str.indexOf(' ');
-
-    // If there is no space, return the entire string
-    if (spaceIndex === -1) {
-        return str;
-    }
-
-    // Return the substring from the start to the first space
-    return str.substring(0, spaceIndex);
+	const trimstr = str.trim();
+	const words = trimstr.split(" ");
+	return words[0];
 }
 
 // Do not change the code below
 
-const s = prompt("Enter String:");
-alert(firstWord(s));
+//const s = prompt("Enter String:");
+//alert(firstWord(s));

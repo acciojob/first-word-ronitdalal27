bf0bf49d38f1f079ed5456f4bf41802ff265f754
ronitdalal27 <dalalronit131@gmail.com>
2025-01-09 12:40:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
 function firstWord(str) {
-  // your code here
-  return str.split(' ')[0];
+      // Check if the input string is empty
+    if (str.length === 0) {
+        return str;
+    }
+
+    // Find the index of the first space
+    const spaceIndex = str.indexOf(' ');
+
+    // If there is no space, return the entire string
+    if (spaceIndex === -1) {
+        return str;
+    }
+
+    // Return the substring from the start to the first space
+    return str.substring(0, spaceIndex);
 }
 
 // Do not change the code below
